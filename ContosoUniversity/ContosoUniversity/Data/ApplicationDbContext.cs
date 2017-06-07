0e80b37101614092809b72471d602419f61728cf
tpreston3 <thad.preston@gmail.com>
2017-06-06 21:29:32 -0600

    Adding new Class work Project

---
+++ ContosoUniversity/ContosoUniversity/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Course>().ToTable("Course");
+            builder.Entity<Student>().ToTable("Student");
+            builder.Entity<Student>().ToTable("Enrollment");
+
+
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage ="Get a shorter last name must be less than 50 Characters")]
+        [RegularExpression(@"^[A-Z].*$", ErrorMessage ="First Letter must be capital letter")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "Get a shorter First name must be less than 50 Characters")]
+        [RegularExpression(@"^[A-Z].*$", ErrorMessage = "First Letter must be capital letter")]
+        public string  FirstMidName { get; set; }
+
+        [Required]
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date, ErrorMessage ="Please enter a valid date (YYYY/MM/DD)")]
+        [DisplayFormat(ApplyFormatInEditMode=true, DataFormatString ="{0:yyyy-MM-dd}")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+        
+    }
+}
